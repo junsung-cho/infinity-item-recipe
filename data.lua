23820f1f5dbb7d2bbade75888faeb3b631153ec8
Junsung Cho <97071544+junsung-cho@users.noreply.github.com>
2024-10-31 17:02:43 +0900

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,20 @@
+data:extend(
+    {
+        {
+            type = "recipe",
+            name = "infinity-chest-recipe",
+            enabled = true,
+            ingredients = {{type = "item", name = "wooden-chest", amount = 1}},
+            results = {{type = "item", name = "infinity-chest", amount = 1}},
+            localised_name = {"item-name.infinity-chest"}
+        },
+        {
+            type = "recipe",
+            name = "infinity-pipe-recipe",
+            enabled = true,
+            ingredients = {{type = "item", name = "pipe", amount = 1}},
+            results = {{type = "item", name = "infinity-pipe", amount = 1}},
+            localised_name = {"item-name.infinity-pipe"}
+        }
+    }
+)
